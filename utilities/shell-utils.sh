e21f9166a5a14a2661380b2c865d649bf803c172
Chris Hillery <ceej@couchbase.com>
2021-01-30 00:59:03 -0800

    CBD-3855: Automation for updating BSL license across a manifest

--- utilities/shell-utils.sh
+++ utilities/shell-utils.sh
@@ -1,9 +1,27 @@
+# Provide a dummy "usage" command for clients that don't define it
+type -t usage || usage() {
+    exit 1
+}
+
 function chk_set {
     var=$1
     # ${!var} is a little-known bashism that says "expand $var and then
     # use that as a variable name and expand it"
     if [[ -z "${!var}" ]]; then
         echo "\$${var} must be set!"
-        exit 1
+        usage
     fi
+}
+
+status() {
+    echo "-- $@"
+}
+
+warn() {
+    echo "!! $@" >&2
+}
+
+error() {
+    warn $@
+    exit 1
 }
